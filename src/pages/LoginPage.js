4313ae8814e730696ab28912158f46ae70c4f21a
Panxoat <a20304645@gmail.com>
2020-08-24 11:54:12 +0900

    [UPDATED] css, input/select tag - component

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import backgroundImg from 'assets/images/login_background.png'
 import LoginSection from 'components/Login'
 import './loginPage.scss'
 const LoginPage = () => {
   return (
     <div className={'container'}>
-      <div>
-        <img src={backgroundImg} alt="loginBackground" className={'background'}></img>
-      </div>
+      <div className={'background'}></div>
       <div>
         <LoginSection />
       </div>
