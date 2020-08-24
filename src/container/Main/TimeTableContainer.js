1204e0e0ffc284382a13e782d546be539a5a95d1
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-08-24 13:12:07 +0900

    [FIX] utc kst time +9 bug

--- src/container/Main/TimeTableContainer.js
+++ src/container/Main/TimeTableContainer.js
@@ -8,7 +8,6 @@ import TimeTableTemp from 'components/TimeTable/TimeTableTemp/TimeTableTemp'
 const TimeTableContainer = observer(() => {
   const { getTimeTable, timeTableList, tableStartTime } = stores.WebinarTimeTableStore
   const { title } = stores.WebinarInfoStore
-  console.log(timeTableList)
 
   const handleGetTimeTable = useCallback(() => {
     getTimeTable().catch((error) => {
@@ -20,18 +19,14 @@ const TimeTableContainer = observer(() => {
     timeTableList &&
     timeTableList.map((data) => {
       const { start_time, end_time, speech, track_name } = data
-      const start_time_min = new Date(start_time).getMinutes()
-      const end_time_min = new Date(end_time).getMinutes()
-      const start_time_hour = new Date(start_time).getHours()
-      const end_time_hour = new Date(end_time).getHours()
-      const min_result = end_time_min - start_time_min
-      const hour_result = (end_time_hour - start_time_hour) * 60
-      const time_result = hour_result + min_result
-
+      const start_time_min = moment.duration(moment(start_time).format('HH:mm'))
+      const end_time_min = moment.duration(moment(end_time).format('HH:mm'))
+      const time_result = (end_time_min - start_time_min) / 60000
+      // console.log(a._milliseconds / 60000)
       return (
         <>
           <tr>
-            <TimeTableTemp art_time={start_time} end_time={end_time} speech={speech} track_name={track_name} result={time_result} />
+            <TimeTableTemp start_time={start_time} end_time={end_time} speech={speech} track_name={track_name} result={time_result} />
           </tr>
         </>
       )
