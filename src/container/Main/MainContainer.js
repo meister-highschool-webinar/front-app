33d7a25b7270bd9cfcd364d40d44e7a7564a73ff
Sh031224 <1cktmdgh2@gmail.com>
2020-08-25 10:26:21 +0900

    [ADD] connect

--- src/container/Main/MainContainer.js
+++ src/container/Main/MainContainer.js
@@ -3,27 +3,53 @@ import { observer, useLocalStore } from 'mobx-react'
 import Info from 'components/Info'
 import TimeTable from 'components/TimeTable'
 import Main from 'components/Main'
+import MainChat from 'components/Main/MainChat'
+import MainSurvey from 'components/Main/MainSurvey'
+import MainLuckydraw from 'components/Main/MainLuckydraw'
+import chatIcon from 'assets/images/chatting-icon@3x.png'
+import surveyIcon from 'assets/images/survey-icon@3x.png'
+import luckydrawIcon from 'assets/images/luckydraw-icon@3x.png'
+import chatActiveIcon from 'assets/images/chatting-active-icon@3x.png'
+import surveyActiveIcon from 'assets/images/survey-active-icon@3x.png'
+import luckydrawActiveIcon from 'assets/images/luckydraw-active-icon@3x.png'
 
-const MainContainer = () => {
+const MainContainer = observer(() => {
   const store = useLocalStore(() => ({
     menuIndex: 0,
     changeMenu: (index) => {
       store.menuIndex = index
     },
+    sideMenuIndex: 0,
+    changeSideMenu: (index) => {
+      store.sideMenuIndex = index
+    },
   }))
 
-  const { menuIndex, changeMenu } = store
+  const { menuIndex, changeMenu, sideMenuIndex, changeSideMenu } = store
 
   const InfoMenus = [
     { title: '정보', contents: Info },
     { title: '타임테이블', contents: TimeTable },
   ]
 
+  const SideMenuInfo = [
+    { title: '채팅', img: chatIcon, active: chatActiveIcon, content: MainChat },
+    { title: '설문결과', img: surveyIcon, active: surveyActiveIcon, content: MainSurvey },
+    { title: '럭키드로우', img: luckydrawIcon, active: luckydrawActiveIcon, content: MainLuckydraw },
+  ]
+
   return (
     <>
-      <Main InfoMenus={InfoMenus} menuIndex={menuIndex} changeMenu={changeMenu} />
+      <Main
+        InfoMenus={InfoMenus}
+        menuIndex={menuIndex}
+        changeMenu={changeMenu}
+        sideMenuIndex={sideMenuIndex}
+        changeSideMenu={changeSideMenu}
+        SideMenuInfo={SideMenuInfo}
+      />
     </>
   )
-}
+})
 
-export default observer(MainContainer)
+export default MainContainer
