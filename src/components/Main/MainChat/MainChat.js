b34b529d668acbcfa3ed7133a51d3afa0b42c653
Panxoat <49806316+Panxoat@users.noreply.github.com>
2020-08-27 11:05:06 +0900

    [ADD] chat_ test step

--- src/components/Main/MainChat/MainChat.js
+++ src/components/Main/MainChat/MainChat.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import io from 'socket.io-client'
 import './MainChat.scss'
 
+const getToken = localStorage.getItem('accessToken')
+const socket = io(`http://54.180.138.80:3000?token=${getToken}`)
+
 const MainChat = () => {
-  return <div>채팅</div>
+  const [chatData, setChatData] = useState('')
+
+  socket.on('connect', () => {
+    console.log('connection')
+
+    socket.on('connected_change', (data) => {
+      console.log('현재 접속 인원', data)
+    })
+  })
+
+  useEffect(() => {
+    console.log('채팅 불러오기!')
+    socket.on('receive message', (chatList) => {
+      console.log('receove', chatList)
+    })
+  })
+
+  const onChange = (e) => {
+    setChatData(e.target.value)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    console.log('content', chatData)
+    socket.emit('send message', (chatData) => {
+      console.log(chatData)
+    })
+  }
+
+  return (
+    <div>
+      <div className={'chatlogBox'}>채팅박스</div>
+      <form onSubmit={onSubmit}>
+        <div className={'chatBox'}>
+          <textarea value={chatData} onChange={onChange} />
+        </div>
+        <div className={'chatEnterArea'}>
+          <button type="submit" className={'chatEnterIcon'}></button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default MainChat
