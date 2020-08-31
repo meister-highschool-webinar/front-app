240adb0bd191757da0b42c3bb1af849d1127b602
Sh031224 <1cktmdgh2@gmail.com>
2020-08-31 15:15:02 +0900

    [DONE] MainSurvey

--- src/components/Main/MainSurvey/MainSurveyPie/MainSurveyPie.js
+++ src/components/Main/MainSurvey/MainSurveyPie/MainSurveyPie.js
@@ -4,14 +4,26 @@ import './MainSurveyPie.scss'
 
 const COLORS = ['#ff6666', '#ff9966', '#ffcc99', '#999999', '#cccccc']
 
-const MainSurveyPie = ({ datas }) => {
+const MainSurveyPie = ({ datas, title }) => {
+  const sorted = datas.slice(0).sort(function (a, b) {
+    return a.count > b.count ? -1 : a.count < b.count ? 1 : 0
+  })
+
+  const getColor = (i) => {
+    return i > 4 ? COLORS[4] : COLORS[i]
+  }
+
   return (
     <div className="main-survey-pie">
       <PieChart width={440} height={200}>
-        <Pie innerRadius={20} outerRadius={90} data={datas} cx={100} cy={100} labelLine={false} fill="#8884d8" dataKey="count" valueKey="name">
-          {datas.map((entry, index) => (
-            <Cell name={entry.name} key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
+        <text style={{ fontWeight: '700' }} x={105} y={105} textAnchor="middle" dominantBaseline="middle">
+          {title}
+        </text>
+        <Pie innerRadius={30} outerRadius={80} data={datas} cx={100} cy={100} labelLine={false} fill="#8884d8" dataKey="count" valueKey="name">
+          {datas.map((entry, index) => {
+            const color = getColor(sorted.indexOf(entry))
+            return <Cell name={entry.name} key={`cell-${index}`} fill={color} />
+          })}
         </Pie>
         <Tooltip />
         <Legend />
