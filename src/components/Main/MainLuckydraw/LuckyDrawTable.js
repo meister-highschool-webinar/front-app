d72abe20e5d69d1f273fa24b13c6a6d672b9111a
hyejun <hyejun0608@naver.com>
2020-09-09 19:09:48 +0900

    LuckyDraw

--- src/components/Main/MainLuckydraw/LuckyDrawTable.js
+++ src/components/Main/MainLuckydraw/LuckyDrawTable.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './LuckyDrawTable.scss';
 import socketio from 'socket.io-client';
 import { SERVER } from '../../../config/config.json';
