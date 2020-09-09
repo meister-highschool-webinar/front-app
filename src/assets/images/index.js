8aabb405f9d78083eaedb83eec3f8e705586c710
hyejun <hyejun0608@naver.com>
2020-09-09 19:06:40 +0900

    LuckyDraw

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,1 @@
 export { default as artwork } from './artwork@3x.png';
-export { default as chattingIcon } from './chatting-icon@3x.png';
-export { default as surveyIcon } from './survey-icon@3x.png';
-export { default as luckydrawIcon } from './luckydraw-icon@3x.png';
--- src/components/Main/MainLuckydraw/MainLuckydraw.js
+++ src/components/Main/MainLuckydraw/MainLuckydraw.js
@@ -7,11 +7,6 @@ const MainLuckydraw = () => {
   return (
     <div className="Background-box">
       <div className="contents">
-        <div className="Icons">
-          <img  src={chattingIcon} className="chatting-icon" />
-          <img src={surveyIcon} className="survey-icon" />
-          <img src={luckydrawIcon} className="luckydraw-icon" />
-        </div>
         <div className="text">럭키드로우</div>
         <div className="Lucky-Draw-Area">
           <img src={artwork} className="Artwork"></img>
@@ -22,4 +17,4 @@ const MainLuckydraw = () => {
   );
 }
 
-export default MainLuckydraw
+export default MainLuckydraw;
