2049b03723780e9427dbbc04462eabf9cc1877ba
zerone2 <yicho93@naver.com>
2020-09-12 22:11:34 +0900

    [build]

--- build/precache-manifest.d95131bf91aed6376efe96130279d20e.js
+++ build/precache-manifest.d95131bf91aed6376efe96130279d20e.js
@@ -1,104 +1,104 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "url": "/0.dc45a3c8175e6aa35ef0.css"
+    url: '/0.dc45a3c8175e6aa35ef0.css',
   },
   {
-    "url": "/2.dc45a3c8175e6aa35ef0.css"
+    url: '/2.dc45a3c8175e6aa35ef0.css',
   },
   {
-    "revision": "4eb262d1354bd02ca19ca40ecd0cc3ea",
-    "url": "/index.html"
+    revision: '4eb262d1354bd02ca19ca40ecd0cc3ea',
+    url: '/index.html',
   },
   {
-    "revision": "8804cfcdfe1b061c682e",
-    "url": "/static/css/2.6df4c8f8.chunk.css"
+    revision: '8804cfcdfe1b061c682e',
+    url: '/static/css/2.6df4c8f8.chunk.css',
   },
   {
-    "revision": "04e2ffeae6b8430f11c8",
-    "url": "/static/css/main.5da87cd5.chunk.css"
+    revision: '04e2ffeae6b8430f11c8',
+    url: '/static/css/main.5da87cd5.chunk.css',
   },
   {
-    "revision": "8804cfcdfe1b061c682e",
-    "url": "/static/js/2.9fcdb6c9.chunk.js"
+    revision: '8804cfcdfe1b061c682e',
+    url: '/static/js/2.9fcdb6c9.chunk.js',
   },
   {
-    "revision": "91d585774034046633666b601f4c7e2f",
-    "url": "/static/js/2.9fcdb6c9.chunk.js.LICENSE.txt"
+    revision: '91d585774034046633666b601f4c7e2f',
+    url: '/static/js/2.9fcdb6c9.chunk.js.LICENSE.txt',
   },
   {
-    "revision": "04e2ffeae6b8430f11c8",
-    "url": "/static/js/main.d8829a44.chunk.js"
+    revision: '04e2ffeae6b8430f11c8',
+    url: '/static/js/main.d8829a44.chunk.js',
   },
   {
-    "revision": "0f0d7a3c8704635b0801",
-    "url": "/static/js/runtime-main.2b4e7897.js"
+    revision: '0f0d7a3c8704635b0801',
+    url: '/static/js/runtime-main.2b4e7897.js',
   },
   {
-    "revision": "aa2342197952fc72ca10bbcc78c0e84d",
-    "url": "/static/media/AnjaEliane.aa234219.ttf"
+    revision: 'aa2342197952fc72ca10bbcc78c0e84d',
+    url: '/static/media/AnjaEliane.aa234219.ttf',
   },
   {
-    "revision": "9d88903fc82980850dc2909d78cb1fad",
-    "url": "/static/media/Recipekorea.9d88903f.ttf"
+    revision: '9d88903fc82980850dc2909d78cb1fad',
+    url: '/static/media/Recipekorea.9d88903f.ttf',
   },
   {
-    "revision": "2e1c85c96e72c8d281fb68fdf51e607a",
-    "url": "/static/media/TalkTalkTalklogo@3x.2e1c85c9.png"
+    revision: '2e1c85c96e72c8d281fb68fdf51e607a',
+    url: '/static/media/TalkTalkTalklogo@3x.2e1c85c9.png',
   },
   {
-    "revision": "4db3a0ce2def34e0b628dcf5a73ac15b",
-    "url": "/static/media/artwork@3x.4db3a0ce.png"
+    revision: '4db3a0ce2def34e0b628dcf5a73ac15b',
+    url: '/static/media/artwork@3x.4db3a0ce.png',
   },
   {
-    "revision": "47c6e2033866d7e81aa393c2e3bfc6a7",
-    "url": "/static/media/background@3x.47c6e203.png"
+    revision: '47c6e2033866d7e81aa393c2e3bfc6a7',
+    url: '/static/media/background@3x.47c6e203.png',
   },
   {
-    "revision": "9c8fad9a2ad6cb45dfd9f5632afe5170",
-    "url": "/static/media/download-button@3x.9c8fad9a.png"
+    revision: '9c8fad9a2ad6cb45dfd9f5632afe5170',
+    url: '/static/media/download-button@3x.9c8fad9a.png',
   },
   {
-    "revision": "39b6fe4f75a7f5a270082e5a456e4abd",
-    "url": "/static/media/home-button@3x.39b6fe4f.png"
+    revision: '39b6fe4f75a7f5a270082e5a456e4abd',
+    url: '/static/media/home-button@3x.39b6fe4f.png',
   },
   {
-    "revision": "1cba9c1a49e12cab49248ca94074ed59",
-    "url": "/static/media/index.1cba9c1a.less"
+    revision: '1cba9c1a49e12cab49248ca94074ed59',
+    url: '/static/media/index.1cba9c1a.less',
   },
   {
-    "revision": "1ef5689c9e52b122b312218e89d611d8",
-    "url": "/static/media/index.1ef5689c.less"
+    revision: '1ef5689c9e52b122b312218e89d611d8',
+    url: '/static/media/index.1ef5689c.less',
   },
   {
-    "revision": "75f988b01df2034ddef55b746adb09c3",
-    "url": "/static/media/index.75f988b0.less"
+    revision: '75f988b01df2034ddef55b746adb09c3',
+    url: '/static/media/index.75f988b0.less',
   },
   {
-    "revision": "c17ec543e0d6a592928535f43867f969",
-    "url": "/static/media/index.c17ec543.less"
+    revision: 'c17ec543e0d6a592928535f43867f969',
+    url: '/static/media/index.c17ec543.less',
   },
   {
-    "revision": "d4327423db1d1c2abdca329c6c9032dc",
-    "url": "/static/media/index.d4327423.less"
+    revision: 'd4327423db1d1c2abdca329c6c9032dc',
+    url: '/static/media/index.d4327423.less',
   },
   {
-    "revision": "a8cfda68e5dc8fae412cd0fce55b2c1c",
-    "url": "/static/media/login_background.a8cfda68.png"
+    revision: 'a8cfda68e5dc8fae412cd0fce55b2c1c',
+    url: '/static/media/login_background.a8cfda68.png',
   },
   {
-    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/static/media/logo.5d5d9eef.svg"
+    revision: '5d5d9eefa31e5e13a6610d9fa7a283bb',
+    url: '/static/media/logo.5d5d9eef.svg',
   },
   {
-    "revision": "39b6fe4f75a7f5a270082e5a456e4abd",
-    "url": "/static/media/logo@3x.39b6fe4f.png"
+    revision: '39b6fe4f75a7f5a270082e5a456e4abd',
+    url: '/static/media/logo@3x.39b6fe4f.png',
   },
   {
-    "revision": "5bc9a2e58d4c2f0dbf3a176c99b1e5ec",
-    "url": "/static/media/start-button@3x.5bc9a2e5.png"
+    revision: '5bc9a2e58d4c2f0dbf3a176c99b1e5ec',
+    url: '/static/media/start-button@3x.5bc9a2e5.png',
   },
   {
-    "revision": "8812ac5c1a290874f2b29d1b5d0d481c",
-    "url": "/static/media/webina-start@3x.8812ac5c.png"
-  }
-]);
+    revision: '8812ac5c1a290874f2b29d1b5d0d481c',
+    url: '/static/media/webina-start@3x.8812ac5c.png',
+  },
+])
