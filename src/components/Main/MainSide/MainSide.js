75e564b3089dbca89d6be9e6d8e39f11cf64ad90
zerone2 <yicho93@naver.com>
2020-09-22 09:41:48 +0900

    [add] add component title to MainSide

--- src/components/Main/MainSide/MainSide.js
+++ src/components/Main/MainSide/MainSide.js
@@ -3,6 +3,7 @@ import './mainSide.scss'
 
 const MainSide = ({ sideMenuIndex, changeSideMenu, SideMenuInfo }) => {
   const MainSideContent = SideMenuInfo[sideMenuIndex].content
+  const title = SideMenuInfo[sideMenuIndex].title
 
   return (
     <div className="main_side">
@@ -11,6 +12,7 @@ const MainSide = ({ sideMenuIndex, changeSideMenu, SideMenuInfo }) => {
           return <img key={i} src={sideMenuIndex === i ? menu.active : menu.img} alt="icon" onClick={() => changeSideMenu(i)} />
         })}
       </div>
+      <div className="main_side_title">{title}</div>
       {MainSideContent}
     </div>
   )
