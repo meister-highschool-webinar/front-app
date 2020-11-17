6b8f2fa361a136014b495d45f3b4e333fc7b2e85
zerone2 <yicho93@gmail.com>
2020-11-18 02:40:35 +0900

    [refactor - Login page style]

--- src/components/Login/NewLogin.js
+++ src/components/Login/NewLogin.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import LoginBtn from './LoginBtn'
 import mainLogo from 'assets/images/webinar-logo@2x.png'
 import './newLogin.scss'
 
 const NewLogin = () => {
   return (
     <div className={'loginSection'}>
-      <img src={mainLogo} alt={'main_logo'} />
-      로그인
+      <img className={'mainLogo'} src={mainLogo} alt={'main_logo'} />
+      <LoginBtn />
     </div>
   )
 }
