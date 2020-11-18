749de0aaa88bacb55871556a695207d137c156d6
zerone2 <yicho93@naver.com>
2020-11-18 12:34:04 +0900

    [add - Signup page]

---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import talkLogo from 'assets/images/logo-talk@2x.png'
+
+const Signup = () => {
+  return (
+    <div className={'signupPage'}>
+      <img src={talkLogo} className={'talkLogo'} alt={'talk-logo'} />
+      <div className={'background'}>history</div>
+    </div>
+  )
+}
+
+export default Signup
