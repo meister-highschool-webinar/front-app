860df2a82525376310de52267537642505427736
zerone2 <yicho93@naver.com>
2020-11-18 13:35:10 +0900

    [style - signup page]

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import talkLogo from 'assets/images/logo-talk@2x.png'
+import './signup.scss'
 
 const Signup = () => {
   return (
     <div className={'signupPage'}>
       <img src={talkLogo} className={'talkLogo'} alt={'talk-logo'} />
-      <div className={'background'}>history</div>
+      <div className={'background'}>
+        <div className={'signupLabel'}>
+          <span className={'engTitle'}><p>Create</p> <p>Account</p></span>
+          <span className={'korTitle'}>회원가입</span>
+        </div>
+      </div>
     </div>
   )
 }
