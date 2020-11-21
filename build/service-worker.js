48ca4c6a5a35811e3f4328ab2677fbbf4ca5b3f1
zerone2 <yicho93@naver.com>
2020-11-22 03:57:49 +0900

    [build - ver 0.2]

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c1e07203ea7d8dee458024e723bf21b.js"
+  "/precache-manifest.84ff1d1c93bb265de247ab30ee9f1341.js"
 );
 
 self.addEventListener('message', (event) => {
