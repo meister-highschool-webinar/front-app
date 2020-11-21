5bfa7fadb18de75b43e322ee9e4bce5c75507bdf
zerone2 <yicho93@naver.com>
2020-11-21 21:31:24 +0900

    [refactor: axios]

--- src/components/Login/LoginBtn.js
+++ src/components/Login/LoginBtn.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import { useGoogleLogin, useGoogleLogout } from 'react-google-login'
 import Swal from 'sweetalert2'
-import { GOOGLE_ID } from 'config/config.json'
+import { GOOGLE_ID, PROD_SERVER } from 'config/config.json'
 import { useStores } from 'stores'
 import { refreshTokenSetup } from 'utils/refreshLoginSetup'
 import 'components/Login/login.scss'
@@ -10,7 +10,7 @@ import googleIcon from 'assets/images/google@2x.png'
 
 const LoginBtn = observer(({ history }) => {
   const {
-    userStore: { accessToken },
+    userStore: { accessToken, userLogout },
   } = useStores()
 
   const onSuccess = (res) => {
@@ -32,22 +32,31 @@ const LoginBtn = observer(({ history }) => {
     console.log('logout success', res)
   }
 
-  const { signIn } = useGoogleLogin({
-    onSuccess,
-    onFailure,
-    clientId: GOOGLE_ID,
-    isSignedIn: true,
-    accessType: 'offline',
-  })
+  // const { signIn } = useGoogleLogin({
+  //   onSuccess,
+  //   onFailure,
+  //   clientId: GOOGLE_ID,
+  //   isSignedIn: true,
+  //   accessType: 'offline',
+  // })
+  //
+  // const { signOut } = useGoogleLogout({
+  //   clientId: GOOGLE_ID,
+  //   onLogoutSuccess,
+  //   onFailure,
+  // })
 
-  const { signOut } = useGoogleLogout({
-    clientId: GOOGLE_ID,
-    onLogoutSuccess,
-    onFailure,
-  })
+  const googleBtnClick = () => {
+    if(accessToken.length === 0) {
+      window.location.href = `${PROD_SERVER}/auth/google`
+      // history.push(`${PROD_SERVER}/auth/google`)
+    } else {
+      userLogout()
+    }
+  }
 
   return (
-    <button onClick={accessToken.length === 0 ? signIn : signOut} className={'loginBtn'}>
+    <button onClick={googleBtnClick} className={'loginBtn'}>
       <img className={'googleIcon'} src={googleIcon} alt={'google'} />
       <span className={'loginBtnText'}>{accessToken.length === 0 ? 'LOGIN with Google' : 'LOGOUT'}</span>
     </button>
